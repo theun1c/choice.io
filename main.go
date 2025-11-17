2c81024d3a334821005d89443337f2792ac1eeaa
theun1c <belovdima1122@yandex.ru>
2025-11-17 06:04:59 +0300

    feat: added unloader path + take 50th most pop animes in 1 json

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/darenliang/jikan-go"
-	"github.com/k0kubun/pp"
+	"github.com/theun1c/unloaded-service_choice.io/unloader"
 )
 
 func main() {
-	jikan.Client.Timeout = time.Second * 10
-	for i := 1; i <= 10; i++ {
-		time.Sleep(10 * time.Second)
-		anime, err := jikan.GetAnimeById(i)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			pp.Println(anime.Data)
-		}
 
-	}
+	un := unloader.NewUnloader()
+	un.Start()
 }
---
+++ unloader/unloader.go
@@ -0,0 +1,30 @@
+package unloader
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/darenliang/jikan-go"
+	"github.com/k0kubun/pp"
+)
+
+type Unloader struct {
+}
+
+func NewUnloader() *Unloader {
+	return &Unloader{}
+}
+
+// take all 50 animes in 1ne json from https://myanimelist.net/topanime.php?type=bypopularity page
+func (u *Unloader) Start() {
+	for i := 1; i <= 1; i++ {
+		anime, err := jikan.GetTopAnime(jikan.TopAnimeTypeTv, "bypopularity", 1) // https://myanimelist.net/topanime.php?type=bypopularity
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			pp.Println(anime.Data)
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
